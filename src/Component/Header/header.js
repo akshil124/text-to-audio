e1399f01165a1031408b578790c5afead9702e61
viral2305 <viral.bvminfotech@gmail.com>
2023-08-28 16:40:05 +0530

    changes footer pages

--- src/Component/Header/header.js
+++ src/Component/Header/header.js
@@ -11,19 +11,6 @@ const Header = () =>{
       <a onClick={()=>navigate('/')}>Home</a>
       <a onClick={()=>navigate('/contact')}>Contact</a>
       <a onClick={()=>navigate('/aboutUs')}>About</a>
-      <div className="container searchbar">
-        <div className="Icon">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-               stroke="#e5e4e5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
-               className="feather feather-search">
-            <circle cx="11" cy="11" r="8"/>
-            <line x1="21" y1="21" x2="16.65" y2="16.65"/>
-          </svg>
-        </div>
-        <div className="InputContainer">
-          <input placeholder="Search for your Voices Card" />
-        </div>
-      </div>
     </div>
   </div><Outlet/><Footer/></>)
 }
